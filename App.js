cc7644d12ebf7cff353a465d4bebeb5fcb48e329
Patrick Rivera <pxr13@my.fsu.edu>
2018-07-12 18:17:55 -0700

    Added header

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './components/Title';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Blah!</Text>
-      </View>
-    );
-  }
-}
+const App = () => (
+  <View>
+    <Title />
+    <Text>Hello</Text>
+  </View>
+);
+// <View>
+//   <Title />
+//   <View>
+//     <Text>Hello!</Text>
+//   </View>
+// </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+export default App;
--- components/Title.js
+++ components/Title.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components';
 
-const styles = StyleSheet.create({
-})
+const TitleWrapper = styled.View`
+  height: 80px;
+  width: 100%;
+  background-color: lightblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TitleText = styled.Text`
+  font-size: 20px;
+  margin-top: 30px;
+`;
 
 export default class Title extends Component {
-
   render() {
-    return null
+    return (
+      <TitleWrapper>
+        <TitleText>Todo List</TitleText>
+      </TitleWrapper>
+    );
   }
 }
